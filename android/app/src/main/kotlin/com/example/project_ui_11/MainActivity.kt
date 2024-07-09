57b8475f4fd8417ea97549fc9c5d9aaab1fa42ce
RaraChairunnisa <rarachairunnisa26@gmail.com>
2024-07-09 17:10:29 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/project_ui_11/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_ui_11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen_page/page_battom_navigation.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:project_ui_11/screen_page/page_home.dart';
+import 'package:project_ui_11/screen_page/page_mycourse.dart';
+
+class PageBottomNavigationBar extends StatefulWidget {
+  const PageBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<PageBottomNavigationBar> createState() => _PageBottomNavigationBarState();
+}
+
+class _PageBottomNavigationBarState extends State<PageBottomNavigationBar> with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          PageHome(),
+          PageMyCourses(),
+          //AksesKamera(),
+          //MapsFlutter(),
+          //PageProfileUser(),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: tabController,
+          labelColor: Colors.green,
+          unselectedLabelColor: Colors.grey,
+          tabs: [
+            Tab(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 4),
+                      child: Icon(Icons.article),
+                    ),
+                    Text("List Berita"),
+                  ],
+                ),
+              ),
+            ),
+            Tab(
+              child: SingleChildScrollView(// Custom layout for Tab
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 4),  // Control spacing between icon and text
+                      child: Icon(Icons.play_arrow),
+                    ),
+                    Text("My Course"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+}
